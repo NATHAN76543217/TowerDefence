43bb49f319da8882db95b3acfb8852cf8a8e7014
Nathan <maximenathan@hotmail.fr>
2021-01-03 20:01:38 +0100

    put screen params on game class

--- game.py
+++ game.py
@@ -1,15 +1,33 @@
-class Game():
-    def __init__(self):
-        self.width = 0
-        self.height = 0
-        return
-    
-    def setWidth(self, width):
-        self.width = width
-    def setHeight(self, Height):
-        self.height = Height
+import pygame
 
-    def getWidth(self):
-        return self.width
-    def getHeight(self):
-        return self.height
+class Game():
+	def __init__(self):
+		self.screen = None
+		self.running = True
+		self.width = 0
+		self.height = 0
+		return
+	
+	def setWidth(self, width):
+		self.width = width
+	def setHeight(self, Height):
+		self.height = Height
+
+	def getWidth(self):
+		return self.width
+	def getHeight(self):
+		return self.height
+	def setScreen(self):
+		self.screen = pygame.display.set_mode((self.getWidth(), self.getHeight()))
+	def getScreen(self):
+		return self.screen
+	def handleEvents(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				self.running = False
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					self.running = False
+				elif event.key == pygame.K_g:
+					self.screen.fill((255, 255, 0))
+		
--- main.py
+++ main.py
@@ -25,24 +25,15 @@ if __name__ == "__main__":
 	readline(gm)
 	background_colour = (255,0,0)
 	print (gm.getWidth(), gm.getHeight())
-	screen = pygame.display.set_mode((gm.getWidth(), gm.getHeight()))
-	# coucou
+	gm.setScreen() 
 	pygame.display.set_caption('Tower defense')
-	screen.fill(background_colour)
+	gm.screen.fill(background_colour)
 	pygame.display.flip()
-	running = True
+
 	BLUE=(0,0,255)
-	pygame.draw.rect(screen, BLUE,(200,150,100,50))
+	pygame.draw.rect(gm.getScreen(), BLUE,(200,150,100,50))
 
-	while running:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running = False
-			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_ESCAPE:
-					running = False
-				elif event.key == pygame.K_g:
-					screen.fill((255, 255, 0))
+	while gm.running:
+		gm.handleEvents()
 		pygame.display.update()
-
 	pygame.quit()
