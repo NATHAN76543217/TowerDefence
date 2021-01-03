7662135cb716a94af77321bce3cf4aa83f881972
MaximeLcail <maximelecaille239@gmail.com>
2021-01-03 19:33:37 +0100

    display blue square

---
+++ game.py
@@ -0,0 +1,15 @@
+class Game():
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+        return
+    
+    def setWidth(self, width):
+        self.width = width
+    def setHeight(self, Height):
+        self.height = Height
+
+    def getWidth(self):
+        return self.width
+    def getHeight(self):
+        return self.height
--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 import pygame
+from game import Game
+#à faire
+#faire setter et getter de sizeH et sizeV
+def readline(game):
+	file = open('maps/map1.td', "r")
+	line = file.readline()
+	split_string = line.split(" ")
+	while line:
+		if split_string[0] == "R":
+			game.setWidth(int(split_string[1]))
+			game.setHeight(int(split_string[2]))
+		#elif split_string[0] == "SH":
+		#	game.setSizeH()
+		line = file.readline()
+		split_string = line.split(" ")
+
+	print (game.getWidth(), game.getWidth())
+	file.close()
+	return game
 
 if __name__ == "__main__":
 	# The background color can be whatever you want
-	(width, height) = (300, 200)
-	background_colour = (0,255,255)
-	screen = pygame.display.set_mode((width, height))
+	gm = Game()
+	readline(gm)
+	background_colour = (255,0,0)
+	print (gm.getWidth(), gm.getHeight())
+	screen = pygame.display.set_mode((gm.getWidth(), gm.getHeight()))
 	# coucou
-	pygame.display.set_caption('Tower defence')
+	pygame.display.set_caption('Tower defense')
 	screen.fill(background_colour)
 	pygame.display.flip()
 	running = True
+	BLUE=(0,0,255)
+	pygame.draw.rect(screen, BLUE,(200,150,100,50))
+
 	while running:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -19,6 +43,6 @@ if __name__ == "__main__":
 					running = False
 				elif event.key == pygame.K_g:
 					screen.fill((255, 255, 0))
-					pygame.display.flip()
+		pygame.display.update()
 
 	pygame.quit()
